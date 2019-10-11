5d7cd8c9ea82e1d7fa0f2ff37516f30cd9ed78ab
smiroshkin <miroshkin.mirus@ya.ru>
2019-10-11 14:57:50 +0300

    create mainapp

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'i+@mj%9a%!msq6we7pt&&f%=c048n78&hk$3v_!1s3w!1c*$5y'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'mainapp',
 ]
 
 MIDDLEWARE = [
